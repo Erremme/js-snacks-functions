52daf99f264be3feaa518f70b575df96c7eeb1b3
Erremme <Erremme2.0@gmail.com>
2024-12-05 21:43:43 +0100

    feat : make  third exercise

---
+++ 03 - conta vocali/script.js
@@ -0,0 +1,58 @@
+/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+
+const word = 'javascript';
+let vowels = 0;
+let consonants = 0;
+
+// Dichiara la funzione qui.
+  const howManyvowels = ( string) =>{
+    let = numberVowels= 0;
+    for (let i = 0; i < string.length; i++) {
+        let caracter = string[i];
+        if (
+            caracter === "a" ||
+            caracter === "e" ||
+            caracter === "i" ||
+            caracter === "o" ||
+            caracter === "u"
+          ) {
+            numberVowels = vowels++
+        }else{
+            consonants++
+        }
+
+             
+  } 
+
+         return numberVowels;
+  }
+
+// Invoca la funzione qui e stampa il risultato in console
+let vowelsNumber = howManyvowels ( word)
+
+
+//Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+    /*/ Verifica se il carattere è una vocale
+    if (
+      carattere === "a" ||
+      carattere === "e" ||
+      carattere === "i" ||
+      carattere === "o" ||
+      carattere === "u"
+    ) {
+      vocali++;
+    } else {
+      consonanti++;
+    }
+  }*/
+
+  console.log(vowels)
+
+
+
+
+
+
+    
